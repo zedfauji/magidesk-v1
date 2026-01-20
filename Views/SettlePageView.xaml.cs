e409c7cd71efc682e7b413e3a5cc0d2cd85293c4
Girish Dudhwal <1310981+zedfauji@users.noreply.github.com>
2026-01-19 19:00:04 -0600

    Auto-commit: Task completed

--- Views/SettlePageView.xaml.cs
+++ Views/SettlePageView.xaml.cs
@@ -1,5 +1,6 @@
 using Magidesk.Presentation.ViewModels;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Navigation;
 
@@ -28,5 +29,36 @@ public sealed partial class SettlePageView : Page
         {
             await ViewModel.InitializeAsync(ticketId);
         }
+    }
+
+    // Quick Cash Click Handlers
+    private void QuickCash1_Click(object sender, RoutedEventArgs e)
+    {
+        ViewModel.QuickCashCommand.Execute(1m);
+    }
+
+    private void QuickCash5_Click(object sender, RoutedEventArgs e)
+    {
+        ViewModel.QuickCashCommand.Execute(5m);
+    }
+
+    private void QuickCash10_Click(object sender, RoutedEventArgs e)
+    {
+        ViewModel.QuickCashCommand.Execute(10m);
+    }
+
+    private void QuickCash20_Click(object sender, RoutedEventArgs e)
+    {
+        ViewModel.QuickCashCommand.Execute(20m);
+    }
+
+    private void QuickCash50_Click(object sender, RoutedEventArgs e)
+    {
+        ViewModel.QuickCashCommand.Execute(50m);
+    }
+
+    private void QuickCash100_Click(object sender, RoutedEventArgs e)
+    {
+        ViewModel.QuickCashCommand.Execute(100m);
     }
 }
